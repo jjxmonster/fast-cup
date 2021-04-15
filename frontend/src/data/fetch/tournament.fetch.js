5bbdebea53a8e934228e9ea783cb9d7d471631c7
jjxmonster <kuba.tarabasz@wp.pl>
2021-04-15 20:08:45 +0200

    fetch

--- frontend/src/data/fetch/tournament.fetch.js
+++ frontend/src/data/fetch/tournament.fetch.js
@@ -1,14 +1,12 @@
 export const fetchTournaments = async () => {
-   const response = await fetch('http://localhost:3030/tournament/all');
+   const response = await fetch('/tournament/all');
    const data = await response.json();
 
    return data;
 };
 
 export const fetchTournamentsTeams = async id => {
-   const response = await fetch(
-      `http://localhost:3030/tournament/${id}/joined-teams`
-   );
+   const response = await fetch(`/tournament/${id}/joined-teams`);
    const data = await response.json();
 
    return data;
